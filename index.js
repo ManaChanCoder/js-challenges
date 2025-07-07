7e7642a6a168b367c8cc3cc2694ad926e635d6fa
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-07 21:06:23 +0800

    Basic Variables

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Goal
+- Practice using let, var, and const
+- Store name, age, course, and hobby
+- Output each one using clg or console.log and use template literals */
+
+const fullName = "Rhogenn Saingga";
+let age = 24;
+let hobby = "Cooking and playing games";
+var course = "BSIT or Bachelor of Science in Information Technology";
+
+console.log(
+  `My name is ${fullName} i am ${age} years old, my course is ${course}, and my hobby is ${hobby}.`
+);
