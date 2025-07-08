d4e8aa0e3ac5093f32928505356c059a800dab4c
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-08 21:18:51 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 /* 
-Create a variable age and assign any number to it.
-Write an if statement to check if the person is 18 or older.
-If true, print: "You are allowed to vote."
-If false, print: "Sorry, you are too young to vote."
+Ask the user to enter two numbers.
+Add the two numbers.
+Print the result like: "The sum of 4 and 5 is 9."
  */
 
-let age = 24;
+const resultElement = document.querySelector("#result");
+const inputElement1 = document.querySelector("#input1");
+const inputElement2 = document.querySelector("#input2");
+const btn = document.querySelector("#add-btn");
 
-if (age >= 18) {
-  console.log("You are allowed to vote.");
-} else if (age < 18) {
-  console.log("Sorry, you are too young to vote.");
-} else {
-  console.log("Invalid age");
-}
+btn.addEventListener("click", () => {
+  const num1 = Number(inputElement1.value);
+  const num2 = Number(inputElement2.value);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    resultElement.textContent = "Please enter valid numbers.";
+    return;
+  } else {
+    const sum = num1 + num2;
+    resultElement.textContent = `${sum}`;
+    inputElement1.value = "";
+    inputElement2.value = "";
+  }
+});
