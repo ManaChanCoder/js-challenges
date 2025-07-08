9c6280827a45600e67943d43898cbcc05426606d
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-08 21:31:12 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 /* 
-Use a for loop to count from 1 to 10.
-Print each number to the console.
+Create an array called favoriteFoods with at least 3 food names inside.
+Use a for loop to print each food to the console.
+Make the output look like:
+" pizza!", " sushi!", etc.
  */
 
-const count = 10;
+const favoriteFoods = [" pizza", " sushi", " pasta"];
+const favoriteDrinks = [" coffee", " tea", " juice"];
+const favoriteDesserts = [" ice cream", " cake", " cookies"];
 
-for (let i = 1; i <= count; i++) {
-  console.log(i);
+for (let i = 0; i < favoriteFoods.length; i++) {
+  console.log(`I love ${favoriteFoods[i]}!`);
+}
+
+favoriteDrinks.forEach((drink) => {
+  console.log(`I love ${drink}!`);
+});
+
+for (favoriteDessert of favoriteDesserts) {
+  console.log(`I love ${favoriteDessert}!`);
 }
