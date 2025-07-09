506426ff9760be78a701482693c049fecd488c15
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 16:38:37 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -2,29 +2,12 @@ const details = {
   firstName: "Rhogenn",
   lastName: "Saingga",
   age: 24,
-  address: {
-    street: "Calavinti st",
-    city: "Rodriguez Montalban",
-    province: "Rizal",
-    zip: "1860",
-  },
+  hobbies: ["coding", "gaming", "reading"],
 };
+const { firstName, lastName, age, hobbies } = details;
 
-details.firstName = "Michael";
-details.lastName = "Gallon";
-details.age = 28;
-details.address.street = "San buena st";
-details.address.city = "Cainta";
-details.address.province = "Rizal";
-details.address.zip = "1900";
+console.log(`My name is ${firstName} ${lastName}, I am ${age} years old.`);
 
-const {
-  firstName,
-  lastName,
-  age,
-  address: { street, city, province, zip },
-} = details;
-
-console.log(
-  `My name is ${firstName} ${lastName}, I am ${age} years old, I live at ${street}, ${city}, ${province}, ${zip}.`
-);
+hobbies.forEach((hobby) => {
+  console.log(`One of Rhogenn's hobbies is ${hobby}.`);
+});
