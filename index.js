04ca4fe453390faa52e66051287a0b78d9a004e9
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 21:10:28 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const students = [
-  { firstName: "Rhogenn", lastName: "Saingga", age: 24, grades: [90, 85, 88] },
-  { firstName: "Aldo", lastName: "Pratama", age: 22, grades: [78, 82, 80] },
-  { firstName: "Budi", lastName: "Santoso", age: 23, grades: [95, 92, 89] },
-  { firstName: "Citra", lastName: "Wulandari", age: 21, grades: [88, 90, 85] },
-  { firstName: "Dewi", lastName: "Lestari", age: 20, grades: [82, 84, 87] },
+  { firstName: "Rhogenn", lastName: "Saingga", age: 20, grades: [90, 85, 88] },
+  { firstName: "Rizky", lastName: "Hidayat", age: 22, grades: [78, 82, 80] },
+  { firstName: "Dimas", lastName: "Pratama", age: 21, grades: [95, 92, 89] },
 ];
 
-const callStudentByName = (lastName) => {
-  const findStudent = students.find((student) => {
-    return student.lastName.toLowerCase() === lastName.toLowerCase();
-  });
-  if (findStudent) {
-    return `Hello, ${findStudent.firstName} ${findStudent.lastName}! You are ${
-      findStudent.age
-    } years old and your grades are ${findStudent.grades.join(", ")}.`;
-  } else {
-    return "Student not found.";
-  }
+const addStudent = (firstName, lastName, age, grades = []) => {
+  const newStudent = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    grades: grades,
+  };
+  students.push(newStudent);
+  console.log(`Student ${firstName} ${lastName} added successfully.`);
 };
 
-console.log(callStudentByName("Saingga"));
+addStudent("Budi", "Santoso", 23, [88, 90, 85]);
+
+students.forEach((student) => {
+  console.log(
+    `Name: ${student.firstName} ${student.lastName}, Age: ${
+      student.age
+    }, Grades: ${student.grades.join(", ")}`
+  );
+});
