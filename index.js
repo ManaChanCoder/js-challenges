4c523cec93cb367829560d5099d9f992f2601320
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 20:20:35 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const items = [
-  {
-    name: "Laptop",
-    quantity: 10,
-    checkStocks() {
-      if (this.quantity > 0) {
-        console.log(`${this.name}: in stock.`);
-      } else {
-        console.log(`${this.name}: out of stock.`);
-      }
-    },
+const calculator = {
+  add(a, b) {
+    return a + b;
   },
-  {
-    name: "Mouse",
-    quantity: 0,
-    checkStocks() {
-      if (this.quantity > 0) {
-        console.log(`${this.name}: in stock.`);
-      } else {
-        console.log(`${this.name}: out of stock.`);
-      }
-    },
+  subtract(a, b) {
+    if (a < b) {
+      return "Subtraction result is negative";
+    } else {
+      return a - b;
+    }
   },
-];
+  multiplication(a, b) {
+    return a * b;
+  },
+  division(a, b) {
+    if (b === 0) {
+      return "Division by zero is not allowed";
+    } else {
+      return a / b;
+    }
+  },
+};
 
-items.forEach((item) => {
-  console.log(item.checkStocks());
-});
+console.log(`${calculator.add(2, 3)}`);
+console.log(`${calculator.subtract(5, 2)}`);
+console.log(`${calculator.multiplication(3, 4)}`);
+console.log(`${calculator.division(10, 2)}`);
