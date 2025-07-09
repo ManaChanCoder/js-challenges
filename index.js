d9ecc0590fc993614f7da427cd3ad55f8d30f38a
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 16:06:03 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 /* 
-Use your existing students array
-Use .find() to get the first student whose course is "Physics"
-If found, print:
-"Found [name]! They are enrolled in Physics."
+Use your existing students array.
+Use .some() to check if there’s at least one student enrolled in "Computer Science".
+If true, print:
+"There is at least one Computer Science student!"
+Else, print:
+"No Computer Science students found."
  */
 
 const students = [
-  { name: "Alice", course: "Mathematics" },
-  { name: "Bob", course: "Physics" },
-  { name: "Charlie", course: "Chemistry" },
-  { name: "David", course: "Physics" },
-  { name: "Eve", course: "Biology" },
+  { name: "Alice", major: "Mathematics" },
+  { name: "Bob", major: "Computer Science" },
+  { name: "Charlie", major: "Physics" },
+  { name: "David", major: "Computer Science" },
+  { name: "Eve", major: "Biology" },
 ];
 
-const foundStudent = students.find((student) => {
-  return student.course === "Physics";
-});
-if (foundStudent) {
-  console.log(`Found ${foundStudent.name}! They are enrolled in Physics.`);
+const hasBiology = students.some((student) => student.major === "Biology");
+
+if (hasBiology) {
+  console.log("There is at least one Biology student!");
+} else {
+  console.log("No Biology students found.");
 }
