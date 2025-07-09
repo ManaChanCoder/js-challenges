3c3a90533efb4b7bc579bd8c6d0085995461e7a1
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 16:23:10 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-/* 
-Use your existing students array.
-Filter the students who are taking "Computer Science".
-Map them into strings like:
-"Hi, I'm [name] and I'm majoring in Computer Science."
-Print each of those strings using .forEach()
- */
+const details = {
+  firstName: "Rhogenn",
+  lastName: "Saingga",
+  age: 24,
+  address: {
+    street: "Calavinti st",
+    city: "Rodriguez Montalban",
+    province: "Rizal",
+    zip: "1860",
+  },
+};
+const {
+  firstName,
+  lastName,
+  age,
+  address: { street, city, province, zip },
+} = details;
 
-const students = [
-  { name: "Alice", major: "Computer Science" },
-  { name: "Bob", major: "Mathematics" },
-  { name: "Charlie", major: "Computer Science" },
-  { name: "David", major: "Physics" },
-  { name: "Eve", major: "Computer Science" },
-];
-
-const csStudents = students.filter((student) => {
-  return student.major === "Computer Science";
-});
-
-const csMessage = csStudents.map((student) => {
-  return `Hi, I'm ${student.name} and I'm majoring in Computer Science.`;
-});
-
-csMessage.forEach((msg) => {
-  console.log(msg);
-});
+console.log(
+  `My name is ${firstName} ${lastName}, I am ${age} years old, I live at ${street}, ${city}, ${province}, ${zip}.`
+);
