760f483fb6b7a658f82e0aa10062f54c0b6342a3
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 19:58:30 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const books = [
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  { title: "1984", author: "George Orwell", year: 1949 },
-];
+const user = {
+  firstName: "Rhogenn",
+  lastName: "Saingga",
+  age: 24,
+  greet() {
+    console.log(
+      `Hello, my name is ${this.firstName} ${this.lastName} and I am ${this.age} years old.`
+    );
+  },
+};
 
-books.forEach((book) => {
-  console.log(
-    `Title: ${book.title}, Author: ${book.author}, Year: ${book.year}`
-  );
-});
+user.greet();
