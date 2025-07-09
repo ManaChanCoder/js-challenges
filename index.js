4b976afed1ad133231ce7f010d9e6cabcd685f99
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-09 20:56:14 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,75 +1,22 @@
 const students = [
-  {
-    firstName: "Rhogenn",
-    lastName: "Saingga",
-    grades: [92, 81, 82, 95, 88],
-    isPassed() {
-      let average = 0;
-      let subjectNumber = 0;
-      this.grades.forEach((grade) => {
-        subjectNumber = this.grades.length;
-        average += grade;
-      });
-      average /= subjectNumber;
-      if (average >= 75) {
-        return `${this.firstName} ${
-          this.lastName
-        } has passed with an average grade of ${average.toFixed(2)}`;
-      } else {
-        return `${this.firstName} ${
-          this.lastName
-        } has not passed with an average grade of ${average.toFixed(2)}`;
-      }
-    },
-  },
-  {
-    firstName: "Rizky",
-    lastName: "Maulana",
-    grades: [85, 90, 78, 88, 92],
-    isPassed() {
-      let average = 0;
-      let subjectNumber = 0;
-      this.grades.forEach((grade) => {
-        subjectNumber = this.grades.length;
-        average += grade;
-      });
-      average /= subjectNumber;
-      if (average >= 75) {
-        return `${this.firstName} ${
-          this.lastName
-        } has passed with an average grade of ${average.toFixed(2)}`;
-      } else {
-        return `${this.firstName} ${
-          this.lastName
-        } has not passed with an average grade of ${average.toFixed(2)}`;
-      }
-    },
-  },
-  {
-    firstName: "Dimas",
-    lastName: "Pratama",
-    grades: [75, 80, 85, 90, 95],
-    isPassed() {
-      let average = 0;
-      let subjectNumber = 0;
-      this.grades.forEach((grade) => {
-        subjectNumber = this.grades.length;
-        average += grade;
-      });
-      average /= subjectNumber;
-      if (average >= 75) {
-        return `${this.firstName} ${
-          this.lastName
-        } has passed with an average grade of ${average.toFixed(2)}`;
-      } else {
-        return `${this.firstName} ${
-          this.lastName
-        } has not passed with an average grade of ${average.toFixed(2)}`;
-      }
-    },
-  },
+  { firstName: "Rhogenn", lastName: "Saingga", age: 24, grades: [90, 85, 88] },
+  { firstName: "Aldo", lastName: "Pratama", age: 22, grades: [78, 82, 80] },
+  { firstName: "Budi", lastName: "Santoso", age: 23, grades: [95, 92, 89] },
+  { firstName: "Citra", lastName: "Wulandari", age: 21, grades: [88, 90, 85] },
+  { firstName: "Dewi", lastName: "Lestari", age: 20, grades: [82, 84, 87] },
 ];
 
-students.forEach((stud) => {
-  console.log(`${stud.isPassed()}`);
-});
+const callStudentByName = (lastName) => {
+  const findStudent = students.find((student) => {
+    return student.lastName.toLowerCase() === lastName.toLowerCase();
+  });
+  if (findStudent) {
+    return `Hello, ${findStudent.firstName} ${findStudent.lastName}! You are ${
+      findStudent.age
+    } years old and your grades are ${findStudent.grades.join(", ")}.`;
+  } else {
+    return "Student not found.";
+  }
+};
+
+console.log(callStudentByName("Saingga"));
