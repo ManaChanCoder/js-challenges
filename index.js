243d7252d9f36ec5c1ef65cbf999e8a097f93577
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-10 15:23:38 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+/* 
+Task / Instruction:
+Gamit ang array na person, gamitin ang .every() method para malaman kung lahat ay adults (age >= 18).
+Kung lahat ay adults, iprint:
+"All persons are adults."
+Kung meron kahit isa na minor, iprint:
+"There are still minors in the list."
+ */
+
 const person = [
   { lastName: "Saingga", firstName: "Rhogenn", age: 24 },
   { lastName: "Sihombing", firstName: "Ricky", age: 25 },
   { lastName: "Sihombing", firstName: "Ricky", age: 25 },
 ];
 
-const findMinor = person.filter((p) => p.age < 18);
+const allAdults = person.every((p) => p.age >= 18);
 
-findMinor.forEach((minor) =>
-  console.log(
-    `Sorry ${minor.firstName}, you are still a minor at ${minor.age} years old`
-  )
-);
+if (allAdults) console.log("All persons are adults.");
+else console.log("There are still minor in the list.");
