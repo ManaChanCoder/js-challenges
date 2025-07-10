3618a9d9f28168565bc336acac928cecea026f73
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-10 14:52:00 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const students = [
-  { firstName: "Rhogenn", lastName: "Saingga", age: 20, grades: [90, 85, 88] },
-  { firstName: "Rizky", lastName: "Hidayat", age: 22, grades: [78, 82, 80] },
-  { firstName: "Dimas", lastName: "Pratama", age: 21, grades: [95, 92, 89] },
+const person = [
+  { lastName: "Saingga", firstName: "Rhogenn", age: 24 },
+  { lastName: "Sihombing", firstName: "Ricky", age: 25 },
+  { lastName: "Sihombing", firstName: "Ricky", age: 25 },
 ];
 
-const addStudent = (firstName, lastName, age, grades = []) => {
-  const newStudent = {
-    firstName: firstName,
-    lastName: lastName,
-    age: age,
-    grades: grades,
-  };
-  students.push(newStudent);
-  console.log(`Student ${firstName} ${lastName} added successfully.`);
+const findPerson = (lastName) => {
+  const found = person.find((p) => {
+    return p.lastName.toLowerCase() === lastName.toLowerCase();
+  });
 };
-
-addStudent("Budi", "Santoso", 23, [88, 90, 85]);
-
-students.forEach((student) => {
-  console.log(
-    `Name: ${student.firstName} ${student.lastName}, Age: ${
-      student.age
-    }, Grades: ${student.grades.join(", ")}`
-  );
-});
+findPerson("Saingga");
