9915c8f6e9bc8b89869006ca7c4679d99018a7d8
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-17 22:54:40 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ class Person {
     this.name = name;
     this.age = age;
   }
-
-  greet = () => {
-    console.log(`Hi i'm ${this.name}, ${this.age} years old`);
-  };
+  static compareAge(personA, personB) {
+    if (personA.age < personB.age) return `Person A is younger than person B`;
+    else if (personA.age > personB.age)
+      return `Person A is Older than person B`;
+    else return `They're both same age`;
+  }
 }
 
-const person1 = new Person("Rhogenn", 24);
-const person2 = new Person("Rodney", 24);
-const person3 = new Person("Joshua", 25);
-const person4 = new Person("Kc", 25);
-const person5 = new Person("Brent", 25);
-const person6 = new Person("Michael", 28);
-person1.greet();
-person6.greet();
+const personA = new Person("Rhogenn", 24);
+const personB = new Person("Joshua", 25);
+console.log(Person.compareAge(personA, personB));
