219fb86063d8cfab2099e274739a934854ea0ab0
ManachanCoder <sainggarhogenn@gmail.com>
2025-07-18 01:36:40 +0800

    Initial Commit - Book Tracker

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-class BankAccount {
-  constructor(owner, balance) {
-    this._owner = owner;
-    this._balance = balance;
+class Book {
+  constructor(title, author, isAvailable = true) {
+    this.title = title;
+    this.author = author;
+    this.isAvailable = isAvailable;
   }
-
-  get accountInfo() {
-    return `Owner: ${this._owner} | Balance: ${this._balance}`;
-  }
-  set owner(owner) {
-    if (owner.length > 0) {
-      this._owner = owner;
-    } else {
-      console.log("Cannot Accept Empty");
-    }
-  }
-
-  withdraw(funds) {
-    if (this._balance > 0) {
-      this._balance -= funds;
-      console.log(`Withdraw successfully. New balance: ${this._balance}`);
-    } else {
-      console.log(`Not enough balance: ${this._balance}`);
-    }
-  }
-  deposit(funds) {
-    this._balance += funds;
-    console.log(`Deposit successfully. New balance: ${this._balance}`);
+  toggleAvailability() {
+    this.isAvailable = !this.isAvailable;
   }
 }
 
-const bankOwner1 = new BankAccount("Rhogenn", 20000);
-bankOwner1.withdraw(12000);
-bankOwner1.deposit(32000);
-console.log(bankOwner1.accountInfo);
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+  listAvailableBooks() {
+    const showBook = this.books.filter((book) => {
+      return book.isAvailable;
+    });
+    showBook.forEach((book) => {
+      console.log(`Title: ${book.title} by Author is ${book.author}`);
+    });
+  }
+  borrowBook(title) {
+    const foundBook = this.books.find(
+      (book) => book.title === title && book.isAvailable
+    );
+
+    if (foundBook) {
+      foundBook.toggleAvailability();
+    } else {
+      console.log(`Sorry, ${title} is currently not available`);
+    }
+  }
+}
+const lib = new Library();
+lib.addBook(new Book("Atomic Habits", "James Clear"));
+lib.addBook(new Book("Deep Work", "Cal Newport"));
+lib.listAvailableBooks();
+
+lib.borrowBook("Atomic Habits");
+lib.listAvailableBooks();
